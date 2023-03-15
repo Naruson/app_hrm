33ce871573abdeba3c8bb88349bfa4a71434f2d8
Piyathidawa <95482092+Piyathidawa@users.noreply.github.com>
2023-03-15 17:46:57 +0700

    edit detailEmployee

--- lib/pages/detailEmployee.dart
+++ lib/pages/detailEmployee.dart
@@ -77,10 +77,90 @@ Future<void> _initializeData() async {
                 ),
               ],
             ),
-      body: Column(
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(_fullnameTH),
+          Icon(IconData(0xe491, fontFamily: 'MaterialIcons'), color: Colors.cyan[100], size: 100,),
+          Text(" " + _fullnameTH, style: TextStyle(fontSize: 20),),
+          Text("(" + _nickname + ")",style: TextStyle(fontSize: 20),),
+          SizedBox(height: 10,),
+          Text(" " + _fullnameEN, style: TextStyle(fontSize: 20 , color: Color.fromARGB(255, 46, 53, 121))),
+          SizedBox(height: 20,),
+          Container(
+            width: 400,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+              Text.rich(  
+            TextSpan(
+              children: <InlineSpan>[
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Icon(IconData(0xf498, fontFamily: 'MaterialIcons', matchTextDirection: true)
+                  , color: Color.fromARGB(255, 46, 53, 121), size: 30,)),
+                TextSpan(text:" ข้อมูลส่วนตัว", style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 46, 53, 121),)),
+              ],
+            ),
+            
+          textAlign: TextAlign.left,
+          style: TextStyle(
+          fontSize: 30
+          ),
+        ),
+      SizedBox(height: 15,),
+      Text(" เบอร์โทร: " + _phone,style: TextStyle(fontSize: 12),),
+      SizedBox(height: 10,),
+      Text(" อีเมล: " +  _email, style: TextStyle(fontSize: 12),),
+      SizedBox(height: 10,),
+      Text(" วันเกิด: " + _birthday, style: TextStyle(fontSize: 12),),
+      SizedBox(height: 15,),
+       Text.rich(  
+            TextSpan(
+              children: <InlineSpan>[
+                WidgetSpan(
+                  alignment: PlaceholderAlignment.middle,
+                  child: Icon(IconData(0xe11b, fontFamily: 'MaterialIcons')
+                  , color: Color.fromARGB(255, 46, 53, 121), size: 30,)),
+                TextSpan(text:" ข้อมูลในบริษัท ", style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 46, 53, 121),)),
+              ])),
+              SizedBox(height: 15,),
+      Text(" บริษัท: " + _company, style: TextStyle(fontSize: 12),),
+      SizedBox(height: 10,),
+      Text(" วันที่เริ่มทำงาน :" +  _start_date, style: TextStyle(fontSize: 12),),
+      SizedBox(height: 10,),
+      Text(" แผนก :" + _department,style: TextStyle(fontSize: 12),),
+      SizedBox(height: 10,),
+      Text(" ตำแหน่ง : " + _position, style: TextStyle(fontSize: 12),),  
+      SizedBox(height: 10,),
+      Text(" ประเภทพนักงาน: " + _emp_type, style: TextStyle(fontSize: 12),), 
+            
+            ],
+
+
+            ),
+      
+            
+          ),
+          
         ],
+
+        ),
+        
       ),
     );
  
